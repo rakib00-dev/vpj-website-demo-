91c8b42f6ac855ffad2b338663963c667cd02433
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-02-14 11:52:18 +0600

    added card in examples card

---
+++ src/Components/Examples.jsx
@@ -0,0 +1,37 @@
+import { Link } from 'react-router-dom';
+
+const Examples = () => {
+  return (
+    <section className="mx-auto w-11/12 max-w-7xl my-12 md:my-20">
+      <div className="grid place-items-center gap-10">
+        <h1 className="text-2xl font-bold text-center">Examples</h1>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-4">
+          <ExamplesCard />
+          <ExamplesCard descripion="Companies in the US pharmaceutical industry that are developing GLP-1 drugs. Also apply the following filter: market cap greater than $10b with EBITDA margin 15% and higher." />
+          <ExamplesCard />
+          <ExamplesCard />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Examples;
+
+const ExamplesCard = ({
+  descripion = `Find small or mid-cap companies (market cap greater than $500m and less than $10b) that focus on AI and Machine Learning platforms.`,
+}) => {
+  return (
+    <div className=" transition-all relative grid w-60 p-3 gap-3 h-48 rounded-2xl bg-gray-100 cursor-pointer hover:bg-purple-300 group">
+      <p className="text-sm w-full text-[#734b8a]">{descripion}</p>
+      <div className="absolute bottom-3 right-3 justify-end items-center hidden group-hover:flex">
+        <Link
+          to=""
+          className="py-2 px-4 text-sm text-white bg-blue-500 rounded-full"
+        >
+          Try This
+        </Link>
+      </div>
+    </div>
+  );
+};
--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,125 +1,13 @@
-import { useRef, useState } from 'react';
-import { FaRegArrowAltCircleRight } from 'react-icons/fa';
+import Examples from '../Components/Examples';
+import StartPageHeader from '../Components/StartPageHeader';
 
 const StartPage = () => {
-  const [isFirstBtnActive, setIsFirstBtnActive] = useState(true);
-  const [isSecondBtnActive, setIsSecondBtnActive] = useState(false);
-  const [isThirdBtnActive, setIsThirdBtnActive] = useState(false);
-
   return (
-    <section className="mx-auto w-11/12 max-w-7xl my-12 md:my-20">
-      <div className="grid place-items-center gap-10">
-        <h1
-          className="text-center w-full text-2xl md:text-4xl font-bold md:w-4/5 "
-          style={{ fontFamily: '"Roboto", serif' }}
-        >
-          The{' '}
-          <span className="text-blue-600">AI-powered investment research</span>{' '}
-          platform for better, differentiated market insights
-        </h1>
-        {/* filter */}
-        <div className="capitalize gap-2 flex justify-center items-center w-fit mx-auto bg-gray-100 py-2 px-1 rounded-xl">
-          <button
-            className={
-              isFirstBtnActive
-                ? `capitalize font-extrabold cursor-pointer text-xs text-blue-400 bg-white py-2 px-4 rounded-xl md:text-lg`
-                : `capitalize font-extrabold cursor-pointer text-xs text-black  py-2 px-4 rounded-xl md:text-lg`
-            }
-            onClick={() => {
-              setIsFirstBtnActive(true);
-              setIsSecondBtnActive(false);
-              setIsThirdBtnActive(false);
-            }}
-          >
-            Research a topic
-          </button>
-          <button
-            className={
-              isSecondBtnActive
-                ? `relative capitalize font-extrabold cursor-pointer text-xs text-blue-400 bg-white py-2 px-4 rounded-xl md:text-lg`
-                : `relative capitalize font-extrabold cursor-pointer text-xs text-black  py-2 px-4 rounded-xl md:text-lg`
-            }
-            onClick={() => {
-              setIsFirstBtnActive(false);
-              setIsSecondBtnActive(true);
-              setIsThirdBtnActive(false);
-            }}
-          >
-            Stock screener
-          </button>
-          <button
-            className={
-              isThirdBtnActive
-                ? `relative capitalize font-extrabold cursor-pointer text-xs text-blue-400 bg-white py-2 px-4 rounded-xl md:text-lg`
-                : `relative capitalize font-extrabold cursor-pointer text-xs text-black  py-2 px-4 rounded-xl md:text-lg`
-            }
-            onClick={() => {
-              setIsFirstBtnActive(false);
-              setIsSecondBtnActive(false);
-              setIsThirdBtnActive(true);
-            }}
-          >
-            Find Companies
-          </button>
-        </div>
-        {/* childrens */}
-        <div className="w-full">
-          {isFirstBtnActive ? (
-            <ResearchATopic />
-          ) : isSecondBtnActive ? (
-            <ResearchATopic />
-          ) : (
-            <FindCompanies />
-          )}
-        </div>
-      </div>
-    </section>
+    <>
+      <StartPageHeader />
+      <Examples />
+    </>
   );
 };
 
 export default StartPage;
-
-const ResearchATopic = () => {
-  const textarea = useRef();
-  const [final, setFinal] = useState(0);
-
-  function handleText(e) {
-    const charLength = e.target.value.slice(' ').length;
-    const charSpace = e.target.value.split(' ').length;
-    setFinal(charLength - charSpace);
-  }
-
-  return (
-    <div className="w-full border p-4 rounded-xl border-gray-300">
-      <textarea
-        className="font-brand-md px-2 sm:px-4 w-full outline-none focus:bg-white p-4 py-3 text-md resize-none transition-all duration-75 min-h-[80px] sm:min-h-0"
-        // className="w-full"
-        placeholder="What investment theme or idea are you interested in learning about?"
-        spellCheck="false"
-        maxLength={3000}
-        ref={textarea}
-        onChange={handleText}
-      ></textarea>
-      <div className="flex justify-between py-2 px-4 w-full gap-3 bg-white transition-all duration-75 ">
-        <button
-          className="text-white px-3 py-2 text-sm rounded-full"
-          style={{ backgroundImage: 'linear-gradient(180deg,#3a31d8,#003fff)' }}
-        >
-          Add filter +
-        </button>
-        <div className="flex gap-2 text-lg justify-center items-center">
-          <span> {final}/ 3000</span>{' '}
-          <FaRegArrowAltCircleRight className="text-blue-400 text-2xl" />
-        </div>
-      </div>
-    </div>
-  );
-};
-
-const FindCompanies = () => {
-  return (
-    <div>
-      <div></div>
-    </div>
-  );
-};
