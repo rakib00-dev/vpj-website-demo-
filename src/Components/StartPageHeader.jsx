787f76436829be42b77028c5612ca911a662b51c
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-02-15 20:33:26 +0600

    updated the start page header

--- src/Components/StartPageHeader.jsx
+++ src/Components/StartPageHeader.jsx
@@ -1,5 +1,7 @@
 import { useRef, useState } from 'react';
+import { CiSearch } from 'react-icons/ci';
 import { FaRegArrowAltCircleRight } from 'react-icons/fa';
+import { MdOutlineCancel } from 'react-icons/md';
 
 const StartPageHeader = () => {
   const [isFirstBtnActive, setIsFirstBtnActive] = useState(true);
@@ -8,7 +10,7 @@ const StartPageHeader = () => {
 
   return (
     <section className="mx-auto w-11/12 max-w-7xl my-12 md:my-20">
-      <div className="grid place-items-center gap-10">
+      <div className="w-full grid place-items-center gap-10">
         <h1
           className="text-center w-full text-2xl md:text-4xl font-bold md:w-4/5 "
           style={{ fontFamily: '"Roboto", serif' }}
@@ -63,7 +65,7 @@ const StartPageHeader = () => {
           </button>
         </div>
         {/* childrens */}
-        <div className="w-full">
+        <div className="w-full ">
           {isFirstBtnActive ? (
             <ResearchATopic />
           ) : isSecondBtnActive ? (
@@ -117,8 +119,67 @@ const ResearchATopic = () => {
 
 const FindCompanies = () => {
   return (
-    <div>
-      <div></div>
+    <div className="w-full grid gap-15">
+      <div className="flex justify-center items-center gap-4 md:gap-8 border border-blue-400 py-4 px-3 rounded-2xl w-full">
+        <CiSearch className="text-blue-600 font-extrabold text-2xl" />
+        <input
+          type="text"
+          placeholder="Search for any company or ticker symbol"
+          className="w-full h-full outline-none"
+        />
+        <MdOutlineCancel className="text-blue-600 font-extrabold text-2xl" />
+      </div>
+      <div className="w-full items-center p-10 border rounded-2xl">
+        <h1 className="text-2xl font-bold">Browse recent reports</h1>
+        <div className="w-full my-12">
+          <Alphabets />
+        </div>
+        <div className="w-full my-12"></div>
+      </div>
+    </div>
+  );
+};
+
+const Alphabets = () => {
+  const alphabet = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ];
+
+  return (
+    <div className="flex capitalize justify-between w-full">
+      {alphabet.map((e, i) => (
+        <button
+          key={i}
+          className="flex capitalize font-bold cursor-pointer p-2 rounded-lg hover:text-white hover:bg-blue-500"
+        >
+          {e}
+        </button>
+      ))}
     </div>
   );
 };
