2c73f1e32fd5b0bbc6b75c26897a88d6a50ed0df
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-02-16 11:54:56 +0600

    updated the routes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Footer from '../Components/Footer';
 import Hero from '../Components/Hero';
 import ProvidesSection from '../Components/ProvidesSection';
@@ -6,6 +7,15 @@ const HomePage = () => {
   return (
     <>
       <Hero />
+      <div className="grid place-items-center cursor-pointer">
+        <Link
+          to={'/app/start'}
+          className=" py-2 px-4 bg-blue-800 text-2xl text-white w-fit mx-auto rounded-xl cursor-pointer hover:bg-blue-500"
+          style={{ zIndex: 233 }}
+        >
+          Go To App Start Page
+        </Link>
+      </div>
       <Footer />
       {/* <ProvidesSection /> */}
     </>
--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Examples from '../Components/Examples';
 import Footer from '../Components/Footer';
 import Resources from '../Components/Resources';
@@ -9,6 +10,15 @@ const StartPage = () => {
       <StartPageHeader />
       <Examples />
       <Resources />
+      <div className="grid place-items-center cursor-pointer">
+        <Link
+          to={'/index-builder/create/search'}
+          className=" py-2 px-4 bg-blue-800 text-2xl text-white w-fit mx-auto rounded-xl cursor-pointer hover:bg-blue-500"
+          style={{ zIndex: 233 }}
+        >
+          Go To Create Page
+        </Link>
+      </div>
       <Footer />
     </>
   );
